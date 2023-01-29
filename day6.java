44b961e5e105e0c733ad477e7b39a45358c04f87
lathadharmalingam <lathadharmanlingam@gmail.com>
2023-01-29 09:32:22 +0530

    Delete day6.java

--- day6.java
+++
@@ -1,6 +0,0 @@
-//write a program to accept string as a command line argument and prints welcome message with it.
-class Sample{  
-public static void main(String args[]){  
-System.out.println("Welcome"+args[0]);  
-}  
-}  
