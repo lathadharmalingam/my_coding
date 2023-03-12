4c65f7bbcb469c204bc03e6fb621a13b5a70fb6d
lathadharmalingam <lathadharmanlingam@gmail.com>
2023-03-12 16:41:21 +0530

    js coding

---
+++ js/js/script.js
@@ -0,0 +1,18 @@
+/*alert("good morning");
+console.log("latha");
+console.log(17635172937);
+console.log(17635172937);
+console.clear();
+console.log("Hello") 
+console.time("Timer");
+setTimeout(()=> console.log("world"),1000);
+console.timeEnd("Timer");
+console.log("latha");*/
+//datatypes
+var a=3456;
+var fname ="latha";
+var fname ="latha";
+console.log(typeof fname);
+console.log(typeof a);
+
+
